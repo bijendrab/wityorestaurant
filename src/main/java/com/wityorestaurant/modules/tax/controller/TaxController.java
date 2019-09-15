cb188c7332e6b69bcde52e9618a4410357783763
akash-beura <46479601+akash-beura@users.noreply.github.com>
2019-09-15 21:45:37 +0530

    Changed mapping

--- src/main/java/com/wityorestaurant/modules/tax/controller/TaxController.java
+++ src/main/java/com/wityorestaurant/modules/tax/controller/TaxController.java
@@ -5,15 +5,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wityorestaurant.modules.tax.model.TaxProfile;
 import com.wityorestaurant.modules.tax.service.TaxService;
 
-@RestController("/api/tax")
+@RestController
+@RequestMapping("/api/tax")
 @CrossOrigin("*")
 public class TaxController {
 	
@@ -27,13 +31,13 @@ public class TaxController {
     }
     
     @PreAuthorize("hasRole('ROLE_USER')")
-    @PostMapping("/edit-profile")
+    @PutMapping("/edit-profile")
 	public ResponseEntity<TaxProfile> editTaxProfile(@RequestBody TaxProfile profile){
     	return new ResponseEntity<TaxProfile>(taxService.editTaxProfile(profile),HttpStatus.OK);
     }
     
     @PreAuthorize("hasRole('ROLE_USER')")
-    @PostMapping("/delete-profile/{profileId}")
+    @DeleteMapping("/delete-profile/{profileId}")
 	public ResponseEntity<Boolean> deleteTaxProfile(@PathVariable Long profileId){
     	return new ResponseEntity<Boolean>(taxService.deleteTaxProfile(profileId),HttpStatus.OK);
     }
