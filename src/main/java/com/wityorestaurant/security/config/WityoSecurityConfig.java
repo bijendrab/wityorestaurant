513bc293ce1348dc2967a308855982b61a37b2dc
Bijendra Behera <bijendra.behera@oracle.com>
2020-10-01 00:20:21 +0530

    SSL Support- actuator

--- src/main/java/com/wityorestaurant/security/config/WityoSecurityConfig.java
+++ src/main/java/com/wityorestaurant/security/config/WityoSecurityConfig.java
@@ -74,6 +74,7 @@ public class WityoSecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/api/restaurant-config/**").permitAll()
 			.antMatchers("/api/payment/**").permitAll()
 			.antMatchers("/api/billing/**").permitAll()
+			.antMatchers("/actuator/**").permitAll()
 			.anyRequest().authenticated();
 		http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 	}
